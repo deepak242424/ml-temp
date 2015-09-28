21eec6151f5bd0048075d892ee3fc7e525615d7e
Deepak <deepak242424@gmail.com>
2015-09-29 01:09:28 +0530

    reduced features implementaion

--- 2_que.py
+++ 2_que.py
@@ -1,58 +1,11 @@
 import numpy as np
 import sklearn.linear_model as LM
 
-def fill_missing_values(filename):
-    f = open(filename)
-    data = f.readlines()
-    print len(data)
-    data = map(lambda x: x.strip().split(','), data)
-
-    data_mat = np.array(data)
-    labels = data_mat[:,-1]
-    labels = np.asarray(labels, dtype=float)
-    data_mat = data_mat[:,5:-1]
-
-    #fill missing values with zeros
-    for i in range(data_mat.shape[0]):
-        for j in range(data_mat.shape[1]):
-            if data_mat[i][j] == '?':
-                data_mat[i][j] = '0'
-
-    data_mat = np.asarray(data_mat, dtype=float)
-
-    # print data_mat[1][96] # it contains a missig value
-    #fill missing values with average of colums
-    cols_avg = np.zeros(data_mat.shape[1])
-
-    for j in range(data_mat.shape[1]):
-        sum = 0
-        for i in range(data_mat.shape[0]):
-            sum = sum + data_mat[i][j]
-        cols_avg[j] = sum/data_mat.shape[1]
-
-    for j in range(data_mat.shape[1]):
-        sum = 0
-        for i in range(data_mat.shape[0]):
-            if data_mat[i][j] == 0:
-                data_mat[i][j] = cols_avg[j]
-    return data_mat, labels
-
-def make_data_splits(X, Y, num_data_points, train_split):
-    Y = np.reshape(Y,(Y.shape[0],1))
-    X_Y = np.concatenate((X, Y), axis=1)
-    xp = int(num_data_points*train_split)
-    for i in range(5):
-        fname_x = 'CandC-train0' + str(i+1) + '.csv'
-        fname_y = 'CandC-test0' + str(i+1) + '.csv'
-        np.random.shuffle(X_Y)
-        np.savetxt(fname_x, X_Y[:xp], delimiter=',')
-        np.savetxt(fname_y, X_Y[xp:], delimiter=',')
-
-def linear_regression_over_splits(splits):
+def linear_regression_over_splits(splits, prefix):
     rss = []
     for cnt in range(splits):
-        fname_x = 'CandC-train0' + str(cnt+1) + '.csv'
-        fname_y = 'CandC-test0' + str(cnt+1) + '.csv'
+        fname_x = prefix + '-train0' + str(cnt+1) + '.csv'
+        fname_y = prefix + '-test0' + str(cnt+1) + '.csv'
         trdata = np.genfromtxt(fname_x, delimiter = ',')
         testdata = np.genfromtxt(fname_y, delimiter = ',')
 
@@ -68,13 +21,13 @@ def linear_regression_over_splits(splits):
         #print 'Coefficients Learned : ', clf_LR.coef_
     print 'Mean RSS : ', np.mean(rss)
 
-def ridge_regression(splits):
+def ridge_regression(splits, prefix):
     for alp_val in range(10):
         alp_val *= 100
         rss = []
         for cnt in range(splits):
-            fname_x = 'CandC-train0' + str(cnt+1) + '.csv'
-            fname_y = 'CandC-test0' + str(cnt+1) + '.csv'
+            fname_x = prefix + '-train0' + str(cnt+1) + '.csv'
+            fname_y = prefix + '-test0' + str(cnt+1) + '.csv'
             trdata = np.genfromtxt(fname_x, delimiter = ',')
             testdata = np.genfromtxt(fname_y, delimiter = ',')
 
@@ -90,15 +43,12 @@ def ridge_regression(splits):
             #print 'Coefficients Learned : ', clf_LR.coef_
         print 'Mean RSS : ', np.mean(rss)
 
-
-filename = './2_que_data/communities.data'
-data_X, labels_Y = fill_missing_values(filename)
-train_split = .8
 num_splits = 5
-make_data_splits(data_X, labels_Y, data_X.shape[0], train_split)
-
-#linear_regression_over_splits(num_splits)
-#ridge_regression(splits=num_splits)
+#linear_regression_over_splits(num_splits, prefix='CandC')
+#linear_regression_over_splits(num_splits, prefix='Reduced')
+ridge_regression(splits=num_splits, prefix='CandC')
+print '*************************************'
+ridge_regression(splits=num_splits, prefix='Reduced')
 
 
 
